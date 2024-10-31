d106754a37294911b088d2f71a2c30205b1dbcb1
Ivan Zhendorenko <mrreic@gmail.com>
2024-11-01 00:15:39 +0300

    init

---
+++ src/test/java/org/example/hahallohback/HahallohBackApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.hahallohback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class HahallohBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
