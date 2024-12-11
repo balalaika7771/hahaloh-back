6474cb2d6afa99cab11cd0f12ccaecfb101fc4c0
Ivan Zhendorenko <i.zhendorenko@it-alnc.ru>
2024-12-11 22:51:05 +0300

    excludedSecurityFilterChain

--- src/main/java/org/example/hahallohback/security/config/SecurityConfig.java
+++ src/main/java/org/example/hahallohback/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.example.hahallohback.security.filter.JwtAuthenticationFilter;
 import org.example.hahallohback.security.provider.CustomAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -17,7 +18,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
@@ -25,29 +25,48 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
   private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
-
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+  /**
+   * Security filter chain for excluded URLs.
+   * This chain permits all requests to the specified URLs without applying any security filters.
+   */
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  @Order(1) // Ensure this filter chain is evaluated first
+  public SecurityFilterChain excludedSecurityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(AbstractHttpConfigurer::disable) // Отключаем CSRF с использованием нового синтаксиса
+        .securityMatcher("/api/doc-ui/**", "/api/auth/**", "/api/oauth/*", "/v3/api-docs/**")
         .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/api/doc-ui/**").permitAll()
-            .requestMatchers("/api/auth/**").permitAll()
-            .requestMatchers("/api/oauth/*").permitAll()
-            .requestMatchers("/v3/api-docs/**").permitAll()// Разрешаем доступ к эндпоинтам авторизации
-            .anyRequest().authenticated() // Остальные запросы требуют авторизации
+            .anyRequest().permitAll()
         )
-        .exceptionHandling(exceptionHandling ->
-            exceptionHandling.authenticationEntryPoint(customAuthenticationEntryPoint) // Указываем кастомный AuthenticationEntryPoint
-        );
-
-    http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        .csrf(AbstractHttpConfigurer::disable);
 
     return http.build();
   }
 
+  /**
+   * Default security filter chain for all other URLs.
+   * This chain applies security configurations, including JWT authentication.
+   */
+  @Bean
+  @Order(2) // Evaluated after the excluded filter chain
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(AbstractHttpConfigurer::disable) // Disable CSRF
+        .authorizeHttpRequests(authorize -> authorize
+            .anyRequest().authenticated() // All other requests require authentication
+        )
+        .exceptionHandling(exceptionHandling ->
+            exceptionHandling.authenticationEntryPoint(customAuthenticationEntryPoint) // Custom entry point for unauthorized access
+        )
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // Add JWT filter
+
+    return http.build();
+  }
+
+  /**
+   * Configure the AuthenticationManager with custom authentication provider.
+   */
   @Bean
   public AuthenticationManager authenticationManager(HttpSecurity http, CustomAuthenticationProvider customAuthenticationProvider) throws Exception {
     AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
@@ -55,11 +74,11 @@ public class SecurityConfig {
     return authenticationManagerBuilder.build();
   }
 
-
+  /**
+   * Define the password encoder bean.
+   */
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 }
-
-
